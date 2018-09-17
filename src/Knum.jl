052a867a558855adca5c840027243d6dbcadd682
AsahiK <klaus271@outlook.com>
2018-09-16 22:00:48 -0700

    Add details and control output

--- src/Knum.jl
+++ src/Knum.jl
@@ -2,19 +2,45 @@ module Knum
 
 using ForwardDiff
 
-function fixed_point(equa, p, N)
+function fixed_point(equa, p, N = 20, show = false)
+    '''
+    Input:
+    equa: The function you are going to compute
+    p:    First point
+    N:    Max # of iteration
+    show: Whether or not to show calculation process
+
+    Output:
+    Approximation
+    '''
     for i in 1:N+1
-        println(i-1,'\t',p)
+        if show
+            println(i-1,'\t',p)
+        end
         p = equa(p)
     end
+    return p
 end
 
-function newton(equa,p,N,tol = 1e-7)
+function newton(equa, p, N = 20, tol = 1e-7, show = false)
+    '''
+    Input:
+    equa: The function you are going to compute
+    p:    First point
+    N:    Max # of iteration
+    tol:  Tolerance
+    show: Whether or not to show calculation process
+
+    Output:
+    Approximation
+    '''
     for i in 1:N+1
         println(i-1,'\t',p)
         dfp = ForwardDiff.gradient(equa,[p])[1]
         if abs(equa(p)/dfp) < tol
-            println(i,'\t', p - equa(p)/dfp)
+            if show:
+                println(i,'\t', p - equa(p)/dfp)
+            end
             return p - equa(p)/dfp
         else
             p = p - equa(p)/dfp
@@ -23,10 +49,26 @@ function newton(equa,p,N,tol = 1e-7)
     return p
 end
 
-function secant(equa,p0,p1,N, tol = 1e-5)
-    println(0,'\t',p0)
+function secant(equa,p0,p1,N=20, tol = 1e-7 , show = false)
+    '''
+    Input:
+    equa:  The function you are going to compute
+    p0,p1: First  and second point
+    N:     Max # of iteration
+    tol:   Tolerance
+    show:  Whether or not to show calculation process
+
+    Output:
+    Approximation
+
+    '''
+    if show:
+        println(0,'\t',p0)
+    end
     for i in 1:N-1
-        println(i,'\t',p1)
+        if show:
+            println(i,'\t',p1)
+        end
         if abs(p1-p0) < tol
             return p1
         end
@@ -34,17 +76,34 @@ function secant(equa,p0,p1,N, tol = 1e-5)
         p1 = p1 - (equa(p1)*(p1-p0))/(equa(p1)-equa(p0))
         p0 = dumy
     end
-    println(N,'\t',p1)
+    if show:
+        println(N,'\t',p1)
+    end
     return p1
 end
 
-function fpst(equa,p0,p1,N, tol = 1e-7)
+function fpst(equa,p0,p1,N, tol = 1e-7, show = false)
+    '''
+    Input:
+    equa:  The function you are going to compute
+    p0,p1: First  and second point
+    N:     Max # of iteration
+    tol:   Tolerance
+    show:  Whether or not to show calculation process
+
+    Output:
+    Approximation
+    '''
     for i in 1:N+1
-        println(i-1,'\t',p1)
+        if show:
+            println(i-1,'\t',p1)
+        end
         # compute derivative
         dfp = equa(p1) * (p1 - p0) / (equa(p1) - equa(p0))
         if abs(dfp) < tol
-            println(i,'\t', p1 - dfp)
+            if show
+                println(i,'\t', p1 - dfp)
+            end
             return p1 - dfp
         else
             dumy = p1
@@ -52,6 +111,7 @@ function fpst(equa,p0,p1,N, tol = 1e-7)
             p0 = dumy
         end
     end
+    return p1
 end
 
 end # module
